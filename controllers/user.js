899afae8624558800befaaa320c9d3f14a3f5c09
BeTa_DaYz <betadayz888@gmail.com>
2021-03-23 08:01:08 +0100

    Sign out setup

--- controllers/user.js
+++ controllers/user.js
@@ -39,3 +39,9 @@ exports.signin = (req, res) => {
         return res.json({token, user: {_id, email, Lastname, firstName, role}})
     });
 };
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    res.json({ message: "Signout success"});
+};
+
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ const { userSignupValidator } = require("../validator")
 
 router.post('/signup', userSignupValidator, signup);
 router.post("/signin", signin)
-
+router.get("/signout", signout)
 
 
 module.exports = router; 
